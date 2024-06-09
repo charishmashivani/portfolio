fb3ef865cd4e1bce07f0e34d0523f4767cc30d3e
Charishma Shivani <107106804+charishmashivani@users.noreply.github.com>
2024-06-09 22:19:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Frontend Developer" , "Web Developer"],
+    strings: ["Frontend Developer" , "UI/UX Designer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
