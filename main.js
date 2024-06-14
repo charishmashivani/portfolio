ed9d2ee62c1f05b5825654ec5e955304a2aa72d5
Charishma Shivani <107106804+charishmashivani@users.noreply.github.com>
2024-06-14 22:50:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,34 +6,3 @@ var typed = new Typed(".text", {
     loop: true
 });
 
-emailjs.init("xCnTgFhbEPX3a3Qbq");
-
-function sendEmail(event){
-
-    event.preventDefault();
-
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const subject = document.getElementById("subject").value;
-    const message = document.getElementById("message").value;
-
-    if(!name || !email || !subject || !message){
-        alert("Please fill in all the fields!!!");
-        return;
-    }
-
-    emailjs.send("service_yfzd39p", "template_adblkgq",{
-        from_name: name,
-        from_email: email,
-        subject: subject,
-        message: message,
-    })
-    .then(function(response){
-        alert("Your message was sent successfully!!!");
-        document.getElementById("contact-form").reset();
-    },function(error){
-        alert("Oops! Something went wrong")
-    });
-}
-
-document.getElementById("contact-form").addEventListener("submit", sendEmail);
